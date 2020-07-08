455c2d938546fe78fd5560ed368042ab6809582b
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-08 16:06:11 -0400

    implement insert method

--- Implementations/Data_Structures/doubly_linked_list.js
+++ Implementations/Data_Structures/doubly_linked_list.js
@@ -106,4 +106,21 @@ class DoublyLinkedList {
     }
     return false;
   }
+
+  /* Adds a node to the linked list at a specific position */
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length) return !!this.push(val);
+
+    let newNode = new Node(val);
+    let beforeNode = this.get(index - 1);
+    let afterNode = beforeNode.next;
+    beforeNode.next = newNode;
+    newNode.prev = beforeNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+    this.length++;
+    return true;
+  }
 }
