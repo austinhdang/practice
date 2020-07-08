4ac81d42de00700fdf3090e0eaf0af95b95a5ed2
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-07 20:28:27 -0400

    implement push method

--- Implementations/Data_Structures/doubly_linked_list.js
+++ Implementations/Data_Structures/doubly_linked_list.js
@@ -12,4 +12,19 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  /* Adds a node to the end of the linked list */
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
