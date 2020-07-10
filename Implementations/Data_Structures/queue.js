fe8de90aaf263139831dc1c131cf720981e449b8
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-10 18:46:17 -0400

    implement queue data structure and its methods

---
+++ Implementations/Data_Structures/queue.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /* Adds a node to the queue */
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  /* Removes a node from the queue */
+  dequeue() {
+    if (!this.first) return null;
+    let oldFirst = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return oldFirst.value;
+  }
+}
