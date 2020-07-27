48c89dfc4866f4a5b96939c0e95f241e96e63208
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-27 19:21:59 -0400

    add Priority Queue implementation

---
+++ Implementations/Data_Structures/priority_queue.js
@@ -0,0 +1,77 @@
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  /* Adds a node to the priority queue */
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  /* Helper method to place the inserted element into the correct index */
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  /* Removes the highest priority node from the priority queue and returns the
+     value */
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+
+  /* Helper method to place the new min into the first index  */
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swapIdx = null;
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swapIdx = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swapIdx === null && rightChild.priority < element.priority) ||
+          (swapIdx !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swapIdx = rightChildIdx;
+        }
+      }
+      if (swapIdx === null) break;
+      this.values[idx] = this.values[swapIdx];
+      this.values[swapIdx] = element;
+      idx = swapIdx;
+    }
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
