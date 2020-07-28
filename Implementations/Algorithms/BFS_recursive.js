541a89b07dc9d97f8a8ff8bf924631f71bbbe934
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-28 19:21:32 -0400

    add recursive BFS implementation

---
+++ Implementations/Algorithms/BFS_recursive.js
@@ -0,0 +1,58 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(v1, v2) {
+    if (v1 in this.adjacencyList && v2 in this.adjacencyList) {
+      this.adjacencyList[v1].push(v2);
+      this.adjacencyList[v2].push(v1);
+    }
+  }
+
+  removeEdge(v1, v2) {
+    if (v1 in this.adjacencyList && v2 in this.adjacencyList) {
+      this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+      this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+    }
+  }
+
+  removeVertex(vertex) {
+    if (vertex in this.adjacencyList) {
+      let neighbors = this.adjacencyList[vertex];
+      for (let neighbor of neighbors) {
+        this.removeEdge(vertex, neighbor);
+      }
+      delete this.adjacencyList[vertex];
+    }
+  }
+
+  bfs(root) {
+    if (!root || !this.adjacencyList[root]) return null;
+    const queue = [ root ];
+    const results = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    function helper() {
+      if (queue.length) {
+        let curr = queue.shift();
+        visited[curr] = true;
+        results.push(curr);
+        adjacencyList[curr].forEach((neighbor) => {
+          if (!visited[neighbor]) {
+            visited[neighbor] = true;
+            queue.push(neighbor);
+          }
+        });
+        helper();
+      }
+    }
+    helper();
+    return results;
+  }
+}
