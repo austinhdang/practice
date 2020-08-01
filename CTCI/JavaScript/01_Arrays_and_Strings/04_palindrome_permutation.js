3e99b813ecd3d99c43e7d448f15ae40f3fedc2cb
Austin <65687945+austinhdang@users.noreply.github.com>
2020-08-01 19:15:02 -0400

    add JS solution for CTCI Q1.4

---
+++ CTCI/JavaScript/01_Arrays_and_Strings/04_palindrome_permutation.js
@@ -0,0 +1,30 @@
+/**
+ * Problem:
+ * Given a string, write a function to check if it is a permutation of a
+ * palindrome. A palindrome is a word or phrase that is the same forwards and
+ * backwards. A permutation is a rearrangement of letters. The palindrome does
+ * not need to be limited to just dictionary words.
+ * 
+ * Example:
+ * Input:  "Tact Coa"
+ * Output: True (permutations: "taco cat", "atco cta", etc.)
+ * 
+ * @param  {string} str The string to analyze
+ * @return {boolean}    True if input string is a permuation of a palindrome
+ *                      (ignoring spaces). False otherwise.
+ */
+
+function isPalindromePermutation(str) {
+  let chars = new Set();
+  // loop through string
+  for (let char of str) {
+    // ignore spaces
+    if (char !== ' ') {
+      // add character if it doesn't exist in the set
+      if (!chars.has(char)) chars.add(char);
+      // delete character if it exists in the set
+      chars.delete(char);
+    }
+  }
+  return chars.size <= 1;
+}
