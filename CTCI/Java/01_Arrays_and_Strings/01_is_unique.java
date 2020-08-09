03ab9c7c2f2c044231c4f5ebfc958d6d85e85f7c
Austin <65687945+austinhdang@users.noreply.github.com>
2020-08-09 11:43:04 -0400

    add Java solution for CtCI Q1.1

---
+++ CTCI/Java/01_Arrays_and_Strings/01_is_unique.java
@@ -0,0 +1,28 @@
+/**
+ * Problem:
+ * Implement an algorithm to determine if a string has all unique characters.
+ * 
+ * Bonus:
+ * Implement the algorithm without using a data structure.
+ * 
+ * @param {string} str String to analyze
+ * @returns {boolean} True if all unique characters. False otherwise.
+ */
+
+class IsUnique {
+    boolean isUniqueChars(String str) {
+        if (str.length() > 128)
+            return false;
+
+        boolean[] char_set = new boolean[128];
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            // already found this char in string
+            if (char_set[val]) {
+                return false;
+            }
+            char_set[val] = true;
+        }
+        return true;
+    }
+}
