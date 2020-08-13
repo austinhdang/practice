1273949e75f3c8d7de33d27100ac631396689a68
Austin <65687945+austinhdang@users.noreply.github.com>
2020-08-13 12:49:11 -0400

    add solution for Palindrome Number

---
+++ LeetCode/JavaScript/palindrome_number.js
@@ -0,0 +1,42 @@
+/**
+ * Problem:
+ * Determine whether an integer is a palindrome. An integer is a palindrome
+ * when it reads the same backward as forward.
+ * 
+ * Example 1:
+ * Input: 121
+ * Output: true
+ * 
+ * Example 2:
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it
+ * becomes 121-. Therefore it is not a palindrome.
+ * 
+ * Example 3:
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ * 
+ * Follow up:
+ * Coud you solve it without converting the integer to a string?
+ * 
+ * @param {number} x Integer to analyze
+ * @return {boolean} True if the integer is read the same backward as forward;
+ */
+
+var isPalindrome = function(x) {
+  // A negative number is not a palindrome
+  if (x < 0) return false;
+  let num = x.toString();
+  let p1 = 0;
+  let p2 = num.length - 1;
+  /* Loop through integer string and compare digits using two pointers.
+   * Start from the outside and work inward. */
+  while (p1 < p2) {
+    if (num[p1] !== num[p2]) return false;
+    p1++;
+    p2--;
+  }
+  return true;
+};
