3239d956044af31ba8070c784f30f321c2b4ac5a
Austin <65687945+austinhdang@users.noreply.github.com>
2020-08-19 16:27:47 -0400

    Add JS solution for CtCI Q2.7

--- CTCI/JavaScript/02_Linked_Lists/07_intersection.js
+++ CTCI/JavaScript/02_Linked_Lists/07_intersection.js
@@ -34,6 +34,26 @@
 function intersection(list1, list2) {
   let len1 = getLength(list1);
   let len2 = getLength(list2);
+  let diff1 = len1 - len2;
+  let diff2 = len2 - len1;
+  /* Skip through lists before the intersection */
+  for (let i = 0; i < diff1; i++) {
+    list1 = list1.next;
+  }
+  /* Skip through lists before the intersection */
+  for (let i = 0; i < diff2; i++) {
+    list2 = list2.next;
+  }
+  let node1 = list1,
+    node2 = list2;
+  /* Step through lists until a node that exists in both lists occurs */
+  while (node1 && node2) {
+    if (node1 === node2) return node1;
+    node1 = node1.next;
+    node2 = node2.next;
+  }
+  // If the end of a list is reached, there's no intersection
+  return undefined;
 }
 
 /* Helper function to get the length of a linked list. */
