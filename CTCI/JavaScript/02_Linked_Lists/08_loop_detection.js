2d398f3ab1f960324c2e26909d204aadb68e1f7d
Austin <65687945+austinhdang@users.noreply.github.com>
2020-08-19 17:57:30 -0400

    add JS set solution for CtCI Q2.8

--- CTCI/JavaScript/02_Linked_Lists/08_loop_detection.js
+++ CTCI/JavaScript/02_Linked_Lists/08_loop_detection.js
@@ -11,21 +11,24 @@
  * Input:  A -> B -> C -> D -> E -> C [the same C as earlier]
  * Output: C
  * 
- * Hints:
- * - There are really two parts to this problem. First, detect if the linked
- *   list has a loop. Second, figure out where the loop starts.
- * - To identify if there's a cycle, try the "runner" approach described on page
- *   93. Have one pointer move faster than the other.
- * - You can use two pointers, one moving twice as fast as the other. If there
- *   is a cycle, the two pointers will collide. They will land at the same
- *   location at the same time. Where do they land? Why there?
- * - If you haven't identified the pattern of where the two pointers start, try
- *   this: Use the linked list 1->2->3->4->5->6->7->8->9->?, where the ? links
- *   to another node. Try making the ? the first node (that is, the 9 points to
- *   1 such that the entire linked list is a loop). Then make the ? the node 2.
- *   Then the node 3. Then the node 4. What is the pattern? Can you explain why
- *   this happens?
+ * N = |list|
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
  * 
  * @param {LinkedList} list The linked list to analyze
  * @return {Node} The node at the beginning of the loop
  */
+
+/* Solution 1: Set Approach */
+function detectLoopStartSet(list) {
+  let seen = new Set();
+  let node = list;
+
+  while (node) {
+    if (seen.has(node)) return node;
+    seen.add(node);
+    node = node.next;
+  }
+
+  return null;
+}
